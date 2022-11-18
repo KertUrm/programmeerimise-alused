e76cffc5e728c2ffbd0b82fe340ee67b15b5129e
KertUrm <118337171+KertUrm@users.noreply.github.com>
2022-11-18 14:03:54 +0200

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,29 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Kui kaugel sa elad kilomeetrites");
+            string kaugus = Console.ReadLine();
+            double kaugusConverted = Convert.ToDouble(kaugus);
+
+            if (kaugusConverted <= 10 && kaugusConverted >= 0)
+            {
+                Console.WriteLine("0 kuni 10");
+            }
+            else if (kaugusConverted <= 30 && kaugusConverted >= 11)
+            {
+                Console.WriteLine("11 kuni 30");
+            }
+            else if (kaugusConverted <= 50 && kaugusConverted >= 31)
+            {
+                Console.WriteLine("31 kuni 50");
+            }
+            else if (kaugusConverted <= 90 && kaugusConverted >= 51)
+            {
+                Console.WriteLine("51 kuni 90");
+            }
+            else if (kaugusConverted >= 91)
+            {
+                Console.WriteLine("Suurem või vördne 91");
         }
     }
 }
