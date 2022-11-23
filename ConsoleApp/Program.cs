a7e4f1aa857ecabb25e1013cd4d38f00330f7934
KertUrm <118337171+KertUrm@users.noreply.github.com>
2022-11-23 11:22:35 +0200

    Update Program.cs

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+using System;
+using System.Runtime.CompilerServices;
 
-namespace ConsoleApp
+namespace ConsoleApp15555
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.White;
+            bool a = true;
+            while (a)
+            {
+                Console.WriteLine("sisesta nimi");
+                string name = Console.ReadLine();
+                if (name != "")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Tere, " + name + " !");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    a = false;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Nime ei sisestatud.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Proovi uuesti.");
+                }
+            }
+            int n1 = 0, n2 = 1, n3;
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            while (n1 < 100)
+            { 
+                n3 = n1 + n2;
+                Console.WriteLine(n3);
+                n1 = n2;
+                n2 = n3;
+            }
         }
     }
 }
